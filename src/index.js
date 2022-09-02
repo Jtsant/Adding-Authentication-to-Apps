baa9e747d13dbcb79f952229d59f738aed8b36f8
Jtsant <jtsant92@hotmail.com>
2022-09-02 15:29:15 +0300

    Managing Auth State with Context

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './store/auth-context';
 
 ReactDOM.render(
+  <AuthContextProvider>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </AuthContextProvider>,
   document.getElementById('root')
 );
