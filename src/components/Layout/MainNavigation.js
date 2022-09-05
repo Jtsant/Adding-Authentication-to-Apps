fd9ecdfd894d0d9cea28dc486bf1cc8cab7ea239
Jtsant <jtsant92@hotmail.com>
2022-09-05 15:28:10 +0300

    Adding Logout

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -9,6 +9,11 @@ const MainNavigation = () => {
 
   const isLoggedIn = authCtx.isLoggedIn;
 
+  const logoutHandler = () => {
+    authCtx.logout();
+    // optional: redirect the user
+  };
+
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -28,7 +33,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
